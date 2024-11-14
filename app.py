7794a2c176bd4e193707b18125682554cd792ab4
tejas-DTskill <tejas.patil@dtskill.com>
2024-11-14 16:39:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class HelloHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello DTSkill")
+
+# Set the server to listen on port 8000
+port = 8000
+server_address = ("", port)
+
+# Create and start the HTTP server
+httpd = HTTPServer(server_address, HelloHandler)
+print(f"Serving on port {port}...")
+httpd.serve_forever()
